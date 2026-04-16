5e23e49bde7bbdba001e0a8883bd1dfae673a580
wan.yu <wan.yu@example.com>
2026-04-16 16:57:23 +0800

    auto commit

--- myscript.sh
+++ myscript.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-
-echo "=== 自动 Git 提交脚本 ==="
-
-# 1. 查看当前改动
+echo "=== 自动 Git 提交 ==="
 git status
-
-# 2. 全部添加
 git add .
-
-# 3. 自动提交（备注可以自己改）
-git commit -m "自动化提交：Shell 脚本练习"
-
-# 4. 推送到远程
+git commit -m "auto commit"
 git push origin HEAD:main
